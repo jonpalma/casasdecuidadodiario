96a2e56590bdd6d8bc24f31c55e1c0fbc1cc92c6
chrisJaquez <chris.jaquez@mixen.mx>
2015-07-08 13:05:02 -0600

    Resultados del sorteo

--- web/sorteo.php
+++ web/sorteo.php
@@ -34,13 +34,18 @@
             <div class="col-sm-4">
                 <img src="img/sorteo.jpg" alt="" class="img-responsive center-block">
             </div>
-            <div class="col-sm-8 padding-top">
-                <p>El 25vo. Sorteo Chihuahua por sus niños se realizará el 26 de Junio a las 13:00 horas en Plaza Galerías.</p>
+            <div class="col-sm-8">
+                <p>El 25vo. Sorteo Chihuahua por sus niños se realizó el 26 de Junio a las 13:00 horas en Plaza Galerías.</p>
                 <p>Premios:</p>
                 <p>1er. Premio es una residencia totalmente amueblada y equipada con un automóvil Spark 2015.</p>
                 <p>2do. Premio es un automóvil Sentra 2015.</p>
                 <p>3er. Premio es un automóvil Aveo 2015.</p>
-                <p>El valor del boleto es de $350.</p>
+				<p>El valor del boleto es de $350. La ganadora fue Lizeth Carolina y/o Edna Georgina.</p>
+				<p>Conoce a los ganadores:</p>
+				
+				<a class="btn btn-orange" style="float:left" href="ganadores.pdf" target="_blank">Descarga la información</a>
+
+
             </div>
         </div>
     </div>
