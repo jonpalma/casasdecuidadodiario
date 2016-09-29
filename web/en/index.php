0b09934b943e90d07054badaf0a6e52b7a9b38c1
PedroBorundaMixen <pedro.borunda@mixen.mx>
2016-09-29 14:34:07 -0600

    Cambio video en seccion Sorteo y eliminar imagen

--- web/en/index.php
+++ web/en/index.php
@@ -1,49 +1,49 @@
 <?php include('modules/header.php'); ?>
 <!-- SLIDE -->
-                           <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                              <ol class="carousel-indicators">
-                                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                                <li data-target="#myCarousel" data-slide-to="1"></li>
-                                <li data-target="#myCarousel" data-slide-to="2"></li>
-                                <li data-target="#myCarousel" data-slide-to="3"></li>
-                              </ol>
-                              <div class="carousel-inner">
-                                <div class="item active">
-                                  <img src="img/banner/5.jpg" class="img-responsive" alt="">
-                                  <div class="container">
-                                    <div class="carousel-caption">
-                                      <h1>“While their parents work, many children are left alone… Support us” </h1>
-                                    </div>
-                                  </div>
-                                </div>
-                               <div class="item">
-                                  <img src="img/banner/2.jpg" class="img-responsive" alt="">
-                                  <div class="container">
-                                    <div class="carousel-caption">
-                                      <h1>“While their parents work, many children are left alone… Support us” </h1>
-                                    </div>
-                                  </div>
-                                </div>
-                                <div class="item">
-                                  <img src="img/banner/3.jpg" class="img-responsive" alt="">
-                                  <div class="container">
-                                    <div class="carousel-caption">
-                                      <h1>“While their parents work, many children are left alone… Support us” </h1>
-                                    </div>
-                                  </div>
-                                </div>
-                                <div class="item">
-                                  <img src="img/banner/4.jpg" class="img-responsive" alt="">
-                                  <div class="container">
-                                    <div class="carousel-caption">
-                                      <h1>“While their parents work, many children are left alone… Support us” </h1>
-                                    </div>
-                                  </div>
-                                </div>
-                             </div>
-                              <a class="left carousel-control" href="#myCarousel" data-slide="prev"><div class="carousel-prev"></div></a>
-                              <a class="right carousel-control" href="#myCarousel" data-slide="next"><div class="carousel-next"></div></a>
-                            </div>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="img/banner/5.jpg" class="img-responsive" alt="">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>“While their parents work, many children are left alone… Support us” </h1>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="img/banner/2.jpg" class="img-responsive" alt="">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>“While their parents work, many children are left alone… Support us” </h1>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="img/banner/3.jpg" class="img-responsive" alt="">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>“While their parents work, many children are left alone… Support us” </h1>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="img/banner/4.jpg" class="img-responsive" alt="">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>“While their parents work, many children are left alone… Support us” </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev"><div class="carousel-prev"></div></a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next"><div class="carousel-next"></div></a>
+</div>
 <!-- END SLIDE -->
 <!-- BEGIN SERVICIOS -->
 <div class="jumbotron bg-white">
@@ -56,33 +56,33 @@
             <div class="col-sm-12 margin-top">
                 <div class="col-sm-1"></div>
                 <div class="col-sm-2">
-                   <div class="square img-responsive">
-                       <img src="img/servicios/cuidado.png" alt="" class="img-responsive center-block">
-                   </div>
+                    <div class="square img-responsive">
+                        <img src="img/servicios/cuidado.png" alt="" class="img-responsive center-block">
+                    </div>
                     <p class="text-center">Care of boys and girls</p>
                 </div>
                 <div class="col-sm-2">
-                   <div class="square img-responsive">
-                       <img src="img/servicios/formacion.png" alt="" class="img-responsive center-block">
-                   </div>
+                    <div class="square img-responsive">
+                        <img src="img/servicios/formacion.png" alt="" class="img-responsive center-block">
+                    </div>
                     <p class="text-center">Integral Formation</p>
                 </div>
                 <div class="col-sm-2">
-                   <div class="square img-responsive">
-                       <img src="img/servicios/nutricion.png" alt="" class="img-responsive center-block">
-                   </div>
+                    <div class="square img-responsive">
+                        <img src="img/servicios/nutricion.png" alt="" class="img-responsive center-block">
+                    </div>
                     <p class="text-center">Nutrition</p>
                 </div>
                 <div class="col-sm-2">
-                   <div class="square img-responsive">
-                       <img src="img/servicios/becas.png" alt="" class="img-responsive center-block img-square">
-                   </div>
+                    <div class="square img-responsive">
+                        <img src="img/servicios/becas.png" alt="" class="img-responsive center-block img-square">
+                    </div>
                     <p class="text-center">Scholarships</p>
                 </div>
                 <div class="col-sm-2">
-                   <div class="square img-responsive">
-                       <img src="img/servicios/auto-empleo.png" alt="" class="img-responsive center-block img-square">
-                   </div>
+                    <div class="square img-responsive">
+                        <img src="img/servicios/auto-empleo.png" alt="" class="img-responsive center-block img-square">
+                    </div>
                     <p class="text-center">Self Employment</p>
                 </div>
                 <div class="col-sm-1"></div>
@@ -90,49 +90,35 @@
             <div class="col-sm-12">
                 <div class="col-sm-1"></div>
                 <div class="col-sm-2">
-                   <div class="square img-responsive">
-                       <img src="img/servicios/musica.png" alt="" class="img-responsive center-block img-square">
-                   </div>
+                    <div class="square img-responsive">
+                        <img src="img/servicios/musica.png" alt="" class="img-responsive center-block img-square">
+                    </div>
                     <p class="text-center">Music</p>
                 </div>
                 <div class="col-sm-2">
-                   <div class="square img-responsive">
-                       <img src="img/servicios/traslado.png" alt="" class="img-responsive center-block img-square">
-                   </div>
+                    <div class="square img-responsive">
+                        <img src="img/servicios/traslado.png" alt="" class="img-responsive center-block img-square">
+                    </div>
                     <p class="text-center">Transportation</p>
                 </div>
                 <div class="col-sm-2">
-                   <div class="square img-responsive">
-                       <img src="img/servicios/ingles.png" alt="" class="img-responsive center-block img-square">
-                   </div>
+                    <div class="square img-responsive">
+                        <img src="img/servicios/ingles.png" alt="" class="img-responsive center-block img-square">
+                    </div>
                     <p class="text-center">English</p>
                 </div>
                 <div class="col-sm-2">
-                   <div class="square img-responsive">
-                       <img src="img/servicios/capacitacion.png" alt="" class="img-responsive center-block img-square">
-                   </div>
+                    <div class="square img-responsive">
+                        <img src="img/servicios/capacitacion.png" alt="" class="img-responsive center-block img-square">
+                    </div>
                     <p class="text-center">Training</p>
                 </div>
                 <div class="col-sm-2">
-                   <div class="square img-responsive">
-                       <img src="img/servicios/computacion.png" alt="" class="img-responsive center-block img-square">
-                   </div>
+                    <div class="square img-responsive">
+                        <img src="img/servicios/computacion.png" alt="" class="img-responsive center-block img-square">
+                    </div>
                     <p class="text-center">Computer Science</p>
                 </div>
-                <div class="col-sm-1"></div>
-            </div>
-            <div class="col-sm-12">
-                <div class="col-sm-1"></div>
-                <div class="col-sm-2"></div>
-                <div class="col-sm-2"></div>
-                <div class="col-sm-2">
-                   <div class="square img-responsive">
-                       <img src="img/servicios/centro.png" alt="" class="img-responsive center-block img-square">
-                   </div>
-                    <p class="text-center">Training Center</p>
-                </div>
-                <div class="col-sm-2"></div>
-                <div class="col-sm-2"></div>
                 <div class="col-sm-1"></div>
             </div>
         </div>
@@ -141,30 +127,30 @@
 <!-- END SERVICIOS -->
 <!-- BEGIN ALIANZAS -->
 <div class="jumbotron bg-purple parallax-container">
-   <div class="parallax"><img src="img/parallax/inicio1.jpg" alt="" class="img-responsive"></div>
+    <div class="parallax"><img src="img/parallax/inicio1.jpg" alt="" class="img-responsive"></div>
     <div class="container alianzas">
         <div class="row">
-           <div class="col-sm-12 margin-bottom">
-               <h2>Alliances</h2>
-               <div class="bottom-white center-block"></div>
-           </div>
+            <div class="col-sm-12 margin-bottom">
+                <h2>Alliances</h2>
+                <div class="bottom-white center-block"></div>
+            </div>
             <div class="col-sm-4">
-               <a href="vinculacion.php#gubernamentales">
+                <a href="vinculacion.php#gubernamentales">
                     <img src="img/alianzas/gob/<?php echo mt_rand(1, 16);?>.png" alt="" class="img-responsive center-block">
                     <h3>Governmental</h3>
-               </a>
+                </a>
             </div>
             <div class="col-sm-4">
-               <a href="vinculacion.php#academicas">
+                <a href="vinculacion.php#academicas">
                     <img src="img/alianzas/aca/<?php echo mt_rand(1, 7);?>.png" alt="" class="img-responsive center-block">
                     <h3>Academic</h3>
-               </a>
+                </a>
             </div>
             <div class="col-sm-4">
-               <a href="vinculacion.php#osc">
+                <a href="vinculacion.php#osc">
                     <img src="img/alianzas/osc/<?php echo mt_rand(1, 9);?>.png" alt="" class="img-responsive center-block">
                     <h3>OSC</h3>
-               </a>
+                </a>
             </div>
         </div>
     </div>
@@ -185,7 +171,7 @@
 <!-- END NOSOTROS -->
 <!-- BEGIN VISITANOS -->
 <div class="bg-mapa">
-  <div class="jumbotron bg-map">
+    <div class="jumbotron bg-map">
         <div class="container visitanos">
             <div class="row">
                 <div class="col-sm-12">
